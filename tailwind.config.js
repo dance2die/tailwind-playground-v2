b01296f16048c5da23f37af0ef1f8887377b7426
Sung M. Kim <sungmkim80@gmail.com>
2021-04-13 03:37:58 -0400

    Disabled JIT mode as it's not working well in a sandbox.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  // uncomment this to enable Just-in-Time mode
+  // https://tailwindcss.com/docs/just-in-time-mode#enabling-jit-mode
+  // mode: "jit",
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {},
   plugins: [],
-}
+  purge: ["./public/index.html"]
+};
